0c6f5d5e88d3057ea5bdb53baec74704a206fe1c
F Santos <borsatole@gmailcom>
2024-06-03 17:25:32 -0300

    novas funções

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+var som_play = document.querySelector("#som_play")
+var som_pause = document.querySelector("#som_pause")
+var som_end = document.querySelector("#som_fim")
+
+var definir_confirmar = document.querySelector("#definir_confirmar")
+var input_time = document.querySelector("#input_time")
+var btn_close = document.querySelector("#modal_fechar")
+
+definir_confirmar.addEventListener("click",()=>{
+    timer.textContent = input_time.value
+    btn_close.click()
+})
+
 var botoes_select = document.querySelectorAll(".btns");
 var timer = document.querySelector("#timer");
 var btn_play = document.querySelector(".btn_play");
@@ -7,6 +20,7 @@ var isStopped = false; // Variável global para controlar a parada do cronômetr
 var countdownTimeout; // Variável para armazenar o ID do timeout
 
 btn_play.addEventListener("click", play);
+
 
 botoes_select[0].addEventListener("click", () => {
     if (botoes_select[0].className == "btns") {
@@ -61,17 +75,18 @@ botoes_select[3].addEventListener("click", () => {
 });
 
 
-
 function play() {
     if (btn_play.textContent === "Play") {
-        btn_play.innerHTML = `<i class='bx bx-pause'></i>Pause`;
+        som_play.play();
+        btn_play.innerHTML = `<i class='bx bxs-skip-next-circle'></i>Pause`;
 
         var minutos = Number(timer.textContent.split(":")[0]);
         var segundos = Number(timer.textContent.split(":")[1]);
         isStopped = false; // Resetar isStopped para false quando iniciar a contagem regressiva
         regressiva(minutos, segundos);
     } else {
-        btn_play.innerHTML = `<i class='bx bx-play'></i>Play`;
+        btn_play.innerHTML = `<i class='bx bxs-caret-right-circle'></i>Play`;
+        som_pause.play();
         isStopped = true; // Definir isStopped para true quando o botão é "Stop"
         clearTimeout(countdownTimeout); // Limpar o timeout
     }
@@ -80,6 +95,8 @@ function play() {
 function regressiva(minutos, segundos) {
     var min = minutos;
     var seg = segundos;
+
+    
 
     function atualizarTempo() {
         if (!isStopped) {
@@ -100,6 +117,10 @@ function regressiva(minutos, segundos) {
                 countdownTimeout = setTimeout(atualizarTempo, 1000); // Armazenar o ID do timeout
             } else {
                 document.getElementById('timer').textContent = "00:00";
+                btn_play.innerHTML = `<i class='bx bx-play'></i>Play`;
+                som_end.play();
+                window.navigator.vibrate(200);
+                timer.textContent = "25:00"
             }
         }
     }
